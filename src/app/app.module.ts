8dc8a409a2d593b7fcfad368bf24293448f3b06d
drapie <j.piedraprog@gmail.com>
2024-06-19 03:50:30 -0400

    feat: first page and tailwind themes config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { NavigationComponent } from './pages/navigation/navigation.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavigationComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
 /** @type {import('tailwindcss').Config} */
+const { createThemes } = require('tw-colors');
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['General Sans', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    createThemes({
+      one: {
+        'primary': '#105717',
+        'background': '#F8F4E1',
+        'accent': '#1E1E1E',
+      },
+      two: {
+        'primary': '#00224D',
+        'background': '#A0153E',
+        'brand': '#000000',
+      },
+      monochrome: {
+        'primary': '#272829',
+        'background': '#FFF6E0',
+        'brand': '#000000',
+      },
+      three:{
+        'primary': '#0C1844',
+        'background': '#C80036',
+        'brand': '#000000',
+      },
+      four:{
+        'primary': '#0C1844',
+        'background': '#FE3C01',
+        'brand': '#000000',
+      }
+    })
+  ],
 }
 
